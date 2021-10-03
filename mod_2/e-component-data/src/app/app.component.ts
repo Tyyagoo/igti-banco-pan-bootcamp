4a3c8166a0d47743d5c501599d9cde5e3d721f74
Tyyagoo <kinuchiha87@gmail.com>
2021-10-02 23:17:52 -0300

    before two-way binding

--- mod_2/e-component-data/src/app/app.component.ts
+++ mod_2/e-component-data/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'e-component-data';
+  movies = [
+    { title: 'Avengers: Ultimato', rating: 0 },
+    { title: 'Avengers: Infinite War', rating: 0 },
+    { title: 'Debi & Loide', rating: 0 },
+    { title: 'Until the Last Man', rating: 0 },
+  ];
 }
