5a5dc5ad59f3a08760644421814ddd139cab2e04
Tyyagoo <kinuchiha87@gmail.com>
2021-10-29 07:26:43 -0300

    trab pratico setup

---
+++ mod_4/trabpratico/src/main/java/com/tyyagoo/trabpratico/TrabpraticoApplication.java
@@ -0,0 +1,13 @@
+package com.tyyagoo.trabpratico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabpraticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabpraticoApplication.class, args);
+	}
+
+}
